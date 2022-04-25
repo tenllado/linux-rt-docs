9bf36b14edeeabd6fadfd674e385f766a7974faf
Christian Tenllado <tenllado@ucm.es>
2022-04-25 18:04:22 +0200

    no index.pdf in build.sh

--- build.sh
+++ build.sh
@@ -7,6 +7,11 @@ do
 	dest=${fname%md}html
 	echo "${src} --> ${dest}"
 	pandoc ${src} -N --self-contained  --template pandoc-templates/toc-sidebarL-title.html-template.html --toc  -o ${dest}
+done
+
+for src in $(ls ../actividades/*.md | grep -v index.md);
+do
+	fname=$(basename $src)
 	dest=${fname%md}pdf
 	echo "${src} --> ${dest}"
 	pandoc ${src} -N --self-contained  -o ${dest}
